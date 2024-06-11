2ac5961cd3262e7cbd48e577c8556e78d5e20573
Luky20 <lukywilan@gmail.com>
2024-06-11 16:33:09 +0700

    upload ui dashboard

--- Ticketing-System-Server/src/java/Class/Acara.java
+++ Ticketing-System-Server/src/java/Class/Acara.java
@@ -32,100 +32,59 @@ public class Acara extends MyModel {
         this.harga = harga;
     }
 
-    /**
-     * @return the id
-     */
     public String getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(String id) {
         this.id = id;
     }
 
-    /**
-     * @return the nama
-     */
     public String getNama() {
         return nama;
     }
 
-    /**
-     * @param nama the nama to set
-     */
     public void setNama(String nama) {
         this.nama = nama;
     }
 
-    /**
-     * @return the kuota
-     */
+
     public int getKuota() {
         return kuota;
     }
 
-    /**
-     * @param kuota the kuota to set
-     */
     public void setKuota(int kuota) {
         this.kuota = kuota;
     }
 
-    /**
-     * @return the lokasi
-     */
     public String getLokasi() {
         return lokasi;
     }
 
-    /**
-     * @param lokasi the lokasi to set
-     */
     public void setLokasi(String lokasi) {
         this.lokasi = lokasi;
     }
 
-    /**
-     * @return the tanggalAcara
-     */
     public Date getTanggalAcara() {
         return tanggalAcara;
     }
 
-    /**
-     * @param tanggalAcara the tanggalAcara to set
-     */
     public void setTanggalAcara(Date tanggalAcara) {
         this.tanggalAcara = tanggalAcara;
     }
 
-    /**
-     * @return the deskripsi
-     */
     public String getDeskripsi() {
         return deskripsi;
     }
 
-    /**
-     * @param deskripsi the deskripsi to set
-     */
     public void setDeskripsi(String deskripsi) {
         this.deskripsi = deskripsi;
     }
 
-    /**
-     * @return the harga
-     */
     public double getHarga() {
         return harga;
     }
 
-    /**
-     * @param harga the harga to set
-     */
     public void setHarga(double harga) {
         this.harga = harga;
     }
