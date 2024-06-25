c14f431c7bf33f16e80dc57b41be37653dec42fe
Dustin Delroy <dustindelroy@gmail.com>
2024-06-25 19:26:45 +0700

    update Model Acara

--- Ticketing-System-Server/src/java/Model/Acara.java
+++ Ticketing-System-Server/src/java/Model/Acara.java
@@ -4,9 +4,7 @@
  */
 package Model;
 
-import Model.Koneksi;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
@@ -17,7 +15,7 @@ import java.util.Date;
  *
  * @author natha
  */
-public class Acara extends Koneksi {
+public class Acara{
 
     private int id;
     private String nama;
@@ -35,8 +33,7 @@ public class Acara extends Koneksi {
         this.tanggalAcara = tanggalAcara;
         this.deskripsi = deskripsi;
         this.harga = harga;
-    }
-    
+    }  
     public Acara() {
         this.id = 0;
         this.nama = "";
@@ -47,16 +44,9 @@ public class Acara extends Koneksi {
         this.harga = 0.0;
     }
 
-    /**
-     * @return the id
-     */
     public int getId() {
         return id;
     }
-
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
@@ -68,8 +58,7 @@ public class Acara extends Koneksi {
     public void setNama(String nama) {
         this.nama = nama;
     }
-
-
+    
     public int getKuota() {
         return kuota;
     }
@@ -86,16 +75,10 @@ public class Acara extends Koneksi {
         this.lokasi = lokasi;
     }
 
-    /**
-     * @return the tanggalAcara
-     */
     public Timestamp getTanggalAcara() {
         return tanggalAcara;
     }
 
-    /**
-     * @param tanggalAcara the tanggalAcara to set
-     */
     public void setTanggalAcara(Timestamp tanggalAcara) {
         this.tanggalAcara = tanggalAcara;
     }
@@ -115,75 +98,80 @@ public class Acara extends Koneksi {
     public void setHarga(double harga) {
         this.harga = harga;
     }
-    public void insertData() {
-        try {
-            Koneksi a = new Koneksi();
-            if (!Koneksi.getConn().isClosed()) {                
-                a.setStatement(Koneksi.getConn().prepareStatement("INSERT INTO Acara(nama, kuota, lokasi, tanggal_acara, deskripsi, harga) VALUES (?,?,?,?,?,?)"));
-                PreparedStatement sql = (PreparedStatement)a.getStatement();
-                sql.setString(1, nama);
-                sql.setInt(2, kuota);
-                sql.setString(3, lokasi);
-                sql.setTimestamp(4, tanggalAcara);
-                sql.setString(5, deskripsi);
-                sql.setDouble(6, harga);
-                sql.executeUpdate();
-                sql.close();
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-    public boolean updateData() {
-        try {
-            Koneksi a = new Koneksi();
-            if (!Koneksi.getConn().isClosed()) {                
-                a.setStatement(Koneksi.getConn().prepareStatement("UPDATE Acara SET nama = ?, kuota = ?, lokasi = ?, tanggal_acara = ?, deskripsi = ?, harga = ? WHERE id = ?")) ;
-                PreparedStatement sql = (PreparedStatement)a.getStatement();
-                sql.setString(1, nama);
-                sql.setInt(2, kuota);
-                sql.setString(3, lokasi);
-                sql.setTimestamp(4, tanggalAcara);
-                sql.setString(5, deskripsi);
-                sql.setDouble(6, harga);
-                sql.setInt(7, id);
-                int rowAffected = sql.executeUpdate();
-                sql.close();
-                return rowAffected != 0;
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        return false;
-    }
-     public void deleteData() {
-        try {
-            Koneksi a = new Koneksi();
-            if (!Koneksi.getConn().isClosed()) {
-                a.setStatement(Koneksi.getConn().prepareStatement("DELETE FROM Acara WHERE id = ?"));
-                PreparedStatement sql = (PreparedStatement)a.getStatement();
-                sql.setInt(1, id);
-                sql.executeUpdate();
-                sql.close();
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+
+// Class Acara tidak perlu insert, update, delete. 
+//    public boolean insertData() {
+//        try {
+//            Koneksi k = new Koneksi();
+//            if (!Koneksi.getConn().isClosed()) {                
+//                k.setStatement(Koneksi.getConn().prepareStatement("INSERT INTO Acara(nama, kuota, lokasi, tanggal_acara, deskripsi, harga) VALUES (?,?,?,?,?,?)"));
+//                PreparedStatement sql = (PreparedStatement)k.getStatement();
+//                sql.setString(1, nama);
+//                sql.setInt(2, kuota);
+//                sql.setString(3, lokasi);
+//                sql.setTimestamp(4, tanggalAcara);
+//                sql.setString(5, deskripsi);
+//                sql.setDouble(6, harga);
+//                int rowAffected = sql.executeUpdate();
+//                sql.close();
+//                
+//                return rowAffected > 0;
+//            }
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        return false;
+//    }
+//    public boolean updateData() {
+//        try {
+//            Koneksi k = new Koneksi();
+//            if (!Koneksi.getConn().isClosed()) {                
+//                k.setStatement(Koneksi.getConn().prepareStatement("UPDATE Acara SET nama = ?, kuota = ?, lokasi = ?, tanggal_acara = ?, deskripsi = ?, harga = ? WHERE id = ?")) ;
+//                PreparedStatement sql = (PreparedStatement)k.getStatement();
+//                sql.setString(1, nama);
+//                sql.setInt(2, kuota);
+//                sql.setString(3, lokasi);
+//                sql.setTimestamp(4, tanggalAcara);
+//                sql.setString(5, deskripsi);
+//                sql.setDouble(6, harga);
+//                sql.setInt(7, id);
+//                int rowAffected = sql.executeUpdate();
+//                sql.close();
+//                return rowAffected != 0;
+//            }
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        return false;
+//    }
+//     public void deleteData() {
+//        try {
+//            Koneksi k = new Koneksi();
+//            if (!Koneksi.getConn().isClosed()) {
+//                k.setStatement(Koneksi.getConn().prepareStatement("DELETE FROM Acara WHERE id = ?"));
+//                PreparedStatement sql = (PreparedStatement)k.getStatement();
+//                sql.setInt(1, id);
+//                sql.executeUpdate();
+//                sql.close();
+//            }
+//        } catch (Exception ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//    }
      public static ArrayList<Acara> viewListData() {
         ArrayList<Acara> collections = new ArrayList<Acara>();
-        Koneksi a = new Koneksi();
+        Koneksi k = new Koneksi();
         try {
-            a.setStatement(Koneksi.getConn().createStatement());
-            a.setResult(a.getStatement().executeQuery("SELECT * FROM Acara"));
-            while (a.getResult().next()) {
-                Acara tampung = new Acara(a.getResult().getInt("id"),
-                        a.getResult().getString("nama"),
-                        a.getResult().getInt("kuota"),
-                        a.getResult().getString("lokasi"),
-                        a.getResult().getTimestamp("tanggal_acara"),
-                        a.getResult().getString("deskripsi"),
-                        a.getResult().getDouble("harga"));
+            k.setStatement((Statement)Koneksi.getConn().createStatement());
+            k.setResult(k.getStatement().executeQuery("SELECT * FROM Acara"));
+            while (k.getResult().next()) {
+                Acara tampung = new Acara(k.getResult().getInt("id"),
+                        k.getResult().getString("nama"),
+                        k.getResult().getInt("kuota"),
+                        k.getResult().getString("lokasi"),
+                        k.getResult().getTimestamp("tanggal_acara"),
+                        k.getResult().getString("deskripsi"),
+                        k.getResult().getDouble("harga"));
                 collections.add(tampung);
             }
             return collections;            
@@ -213,7 +201,7 @@ public class Acara extends Koneksi {
      public static Acara findByDate(Timestamp tgl) {
         Koneksi a = new Koneksi();
         try {
-            a.setStatement(Koneksi.getConn().prepareStatement("SELECT * FROM Acara WHERE ?>=curdate();"));
+            a.setStatement(Koneksi.getConn().prepareStatement("SELECT * FROM Acara WHERE tanggal_acara = ?;"));
             PreparedStatement sql = (PreparedStatement)a.getStatement();
             sql.setTimestamp(1, tgl);
             a.setResult(sql.executeQuery());
